bde12f0af557d952edd4bb8121c72f2e054f7b34
galit-sys <galit@oshri.co.il>
2026-01-04 16:32:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye compose :) there - new web!
+          Hello Expose!
         </p>
         <a
           className="App-link"
